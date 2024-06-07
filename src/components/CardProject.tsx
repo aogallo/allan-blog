e8d6821b70979bf863e885d512d73402a790d902
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-06-06 21:37:03 -0600

    fix styles

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -15,20 +15,20 @@ export default function CardProject({ frontmatter }: CardProjectProps) {
       <div className="">
         {image && (
           <img
-            className="h-48 w-96 object-contain"
+            className="sm:h-35 sm:w-35 h-48 w-96 object-contain"
             src={image.url}
             alt={image.alt}
           />
         )}
       </div>
-      <h2 className="my-2">{title}</h2>
-      <p>{description}</p>
+      <h2 className="my-2 sm:my-1">{title}</h2>
+      <p className="sm:display-none">{description}</p>
       <div className="mt-3 flex flex-row gap-2">
         <a
           target={target}
           href={disabled ? "#" : github}
           // tabindex={disabled ? "-1" : "0"}
-          className="group inline-block hover:text-skin-accent sm:text-skin-accent"
+          className="group inline-block hover:text-skin-accent"
           // aria-label={ariaLabel}
           title={title}
           // aria-disabled={disabled}
